85ed557b7cbb2bfebbb40ba2e4fe23ee9e23afa7
Ardian Maulana <exbeelh@gmail.com>
2023-01-16 12:19:40 +0700

    adding try catch in api

--- src/api.js
+++ src/api.js
@@ -4,11 +4,19 @@ const baseUrl = process.env.REACT_APP_BASEURL;
 const apiKey = process.env.REACT_APP_APIKEY;
 
 export const getPopularMovies = async() => {
-    const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
-    return movie.data.results;
+    try {
+        const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
+        return movie.data.results;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 export const searchMovies = async(query) => {
-    const search = await axios.get(`${baseUrl}/search/movie?query=${query}&api_key=${apiKey}`);
-    return search.data;
+    try {
+        const search = await axios.get(`${baseUrl}/search/movie?query=${query}&api_key=${apiKey}`);
+        return search.data;
+    } catch (err) {
+        console.log(err);
+    }
 }
